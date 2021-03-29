d06f4f86b1d98573fcbfc3d96e1727b7ac70762c
Redseb <mikolajzyzanski@gmail.com>
2021-03-29 13:41:23 +0200

    new version

---
+++ precache-manifest.09ce5c4f447682fbe402d20895dadb95.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c170d9738f6d715d6309f29f04745cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "09c869c954d28685948a",
+    "url": "/static/css/main.50eba458.chunk.css"
+  },
+  {
+    "revision": "8355a120ac95edb49c31",
+    "url": "/static/js/2.7a1f8b4c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7a1f8b4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09c869c954d28685948a",
+    "url": "/static/js/main.57a02e44.chunk.js"
+  },
+  {
+    "revision": "b56e9186848a08b8d2a1",
+    "url": "/static/js/runtime-main.926bdd2b.js"
+  },
+  {
+    "revision": "bd47ccfce89c83e073db415ea6630649",
+    "url": "/static/media/MikołajZyzańskiCV.bd47ccfc.pdf"
+  },
+  {
+    "revision": "c8ad2d031a850ee9838bc6b3e6e17c4a",
+    "url": "/static/media/cpsScreen1.c8ad2d03.png"
+  },
+  {
+    "revision": "8c6556a30722bfd9d55d9220d42bf19e",
+    "url": "/static/media/cpsScreen2.8c6556a3.png"
+  },
+  {
+    "revision": "263ba4f9f780546eacf3ed264ee9dbaf",
+    "url": "/static/media/cpsScreen3.263ba4f9.png"
+  },
+  {
+    "revision": "3746f1183e1be909c2aab145d440bbcc",
+    "url": "/static/media/drinkUpImg.3746f118.webp"
+  },
+  {
+    "revision": "21703392bd7dc335e26adc96e0d122da",
+    "url": "/static/media/drinkUpScreen1.21703392.webp"
+  },
+  {
+    "revision": "2c7eab77f24e5df63e499b28193e2dd6",
+    "url": "/static/media/drinkUpScreen2.2c7eab77.webp"
+  },
+  {
+    "revision": "780d329c1980b9cbb084fe1cce3c16d4",
+    "url": "/static/media/drinkUpScreen3.780d329c.webp"
+  },
+  {
+    "revision": "4e9b3a40cb3d3f01812df813172873b8",
+    "url": "/static/media/mushroomologyImg.4e9b3a40.png"
+  },
+  {
+    "revision": "7decec3db4560c3e05136a09d498f9e9",
+    "url": "/static/media/mushroomologyScreen1.7decec3d.jpg"
+  },
+  {
+    "revision": "0a6262521052072c83e2a64339c1220a",
+    "url": "/static/media/mushroomologyScreen2.0a626252.jpg"
+  },
+  {
+    "revision": "7ed146e7691b65eca40f1bd30aa7e339",
+    "url": "/static/media/mushroomologyScreen3.7ed146e7.jpg"
+  },
+  {
+    "revision": "017fd5b9989aa9ed01adf775678ba1b2",
+    "url": "/static/media/pcImg.017fd5b9.webp"
+  },
+  {
+    "revision": "b7aeb70220f3715dfb3ff41045a14303",
+    "url": "/static/media/pcScreen1.b7aeb702.webp"
+  },
+  {
+    "revision": "f65af9237bb28750934ef612749a146e",
+    "url": "/static/media/pcScreen2.f65af923.webp"
+  },
+  {
+    "revision": "beece398496ffa9a2d13592299570130",
+    "url": "/static/media/pcScreen3.beece398.webp"
+  },
+  {
+    "revision": "165e9249517f6fbb2f2f9cc3a48e2d95",
+    "url": "/static/media/problemonImg.165e9249.png"
+  },
+  {
+    "revision": "f37ec1786bd4170b64c685099e5dec79",
+    "url": "/static/media/problemonScreen1.f37ec178.jpg"
+  },
+  {
+    "revision": "a7f7eab259932febd376835c4daeb770",
+    "url": "/static/media/problemonScreen2.a7f7eab2.jpg"
+  },
+  {
+    "revision": "679288aeba59419dd1026c6e552a7055",
+    "url": "/static/media/problemonScreen3.679288ae.jpg"
+  },
+  {
+    "revision": "3a89dbea25e3e026d64fdad5bbcdcd1a",
+    "url": "/static/media/profile.3a89dbea.jpg"
+  }
+]);
